0fd1f965ee89c27cabaa68a2ecdc3e75d81c9283
Tobias Bühlmann <t.buehlmann@thisisdmg.com>
2014-05-04 21:35:40 +0200

    Removed unused code.

--- lib/tomate.rb
+++ lib/tomate.rb
@@ -1,7 +1,4 @@
 module Tomate
   require 'tomate/timer'
   require 'tomate/version'
-
-  dir = File.dirname(File.realpath(__FILE__))
-  POMODORO_PNG = File.join(dir, 'tomate', 'images', 'pomodoro.png')
 end
